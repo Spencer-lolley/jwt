dec8e11fc4a25c32a5f11d679bf5b8db741d649f
Spencer-lolley <j.hamisi@peponihouse.sc.ke>
2023-05-04 13:34:42 +0300

    'Initialization'

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response 
+from django.http import JsonResponse
+
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+    return JsonResponse(routes, safe=False)
